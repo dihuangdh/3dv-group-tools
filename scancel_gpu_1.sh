64412c59e9b9a25918765ac257225c13dac3341b
dihuangdh <dihuanginfo@gmail.com>
2024-05-10 16:23:09 +0800

    Add squeue and scancel for 1 gpu tasks

---
+++ scancel_gpu_1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+count=0
+tempfile=$(mktemp)
+
+squeue -p Ai4sci_3D | grep 'gpu:1' > "$tempfile"
+
+while read -r line; do
+    echo "$line"
+    # 从行中提取任务ID
+    job_id=$(echo "$line" | awk '{print $1}')
+    # 这里添加你要对每个任务执行的命令
+    scancel "$job_id"
+    # 增加计数器
+    count=$((count + 1))
+done < "$tempfile"
+
+echo "Total lines: $count"
+
+# 删除临时文件
+rm "$tempfile"
